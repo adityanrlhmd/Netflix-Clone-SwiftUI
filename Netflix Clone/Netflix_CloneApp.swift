d69a0506f6e5c69b3f79cce52784bf2cb01ceaa6
adityanrlhmd <adityanurulhamda@gmail.com>
2023-08-16 09:38:13 +0700

    Initial Commit

---
+++ Netflix Clone/Netflix_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneApp.swift
+//  Netflix Clone
+//
+//  Created by Aditt on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
